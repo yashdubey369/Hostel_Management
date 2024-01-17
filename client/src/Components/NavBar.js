834176c164d9e8338185e178c0d9fe7198959441
Shashank <shekharshashank093@gmail.com>
2024-01-17 22:49:04 +0530

    Updated NavBar, Added react-icons

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { ImSearch } from "react-icons/im";
+import { CgProfile } from "react-icons/cg";
+
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div>
-      This is my NavBar
+    <div className="virat">
+      <div className="hai">
+        <ul className="hello">
+          <li>
+            <input type="text" id="fname" name="fname" />
+          </li>
+        </ul>
+      </div>
+      <ImSearch size={20} />
+      <CgProfile className="ji" size={30} />
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
