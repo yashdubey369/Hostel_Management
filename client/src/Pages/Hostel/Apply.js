ef9e277030ad3fdecdbe30a4f8ab807817a40b72
Shashank <shekharshashank093@gmail.com>
2024-01-21 13:38:33 +0530

    Add Apply page. (generated by blackbox.ai )

--- client/src/Pages/Hostel/Apply.js
+++ client/src/Pages/Hostel/Apply.js
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import React from "react";
+import "./Apply.css";
 function Apply() {
   return (
-    <div>Apply</div>
-  )
+    <div className="apply-container">
+      <form className="apply-main" action="#">
+        <h1>Hostel Management Form</h1>
+        <div className="row">
+          <div className="row-mains">
+            First Name
+            <input
+              type="text"
+              id="fname"
+              name="firstname"
+              placeholder="Your First Name.."
+            />
+            Last Name
+            <input
+              type="text"
+              id="lname"
+              name="lastname"
+              placeholder="Your Last Name.."
+            />
+            Phone Number.
+            <input
+              type="number"
+              id="phnnumber"
+              name="phonenumber"
+              placeholder="123-4567-890"
+            />
+            Roll Number
+            <input
+              type="number"
+              id="rollname"
+              name="rollnumber"
+              placeholder="20587090"
+            />
+            Gender
+            <select id="Gender" name="Gender">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="others">Others</option>
+            </select>
+          </div>
+        </div>
+        <div className="row">
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Apply
+export default Apply;
