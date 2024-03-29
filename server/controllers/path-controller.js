35a44237afd70e6a3434473765ec01e014004dec
Yash <dubeycdk@gmail.com>
2024-03-29 19:43:26 +0530

    update

--- server/controllers/path-controller.js
+++ server/controllers/path-controller.js
@@ -1,38 +1,31 @@
-
-const home=async (req,res)=>{
-   try{
-  res.status(200).send("welcome Mohan");
-   }catch(error){
-       console.log(error);
-   }
-}
-const register=async(req,res)=>{
-   try{ 
-    res.status(200).json("hey Mohan ,welcome to registration page");
-
-}
-catch(error){
-  res.status(500).json("hey Mohan ,internal server error");
-
-}
-};
-  const login = async (req,res)=>{
-    try{
-     res.status(200).json("all right , Mohan");
-    }
-    catch(error){
-      res.status(500).json("hey Mohan ,server error");
-    }
-  } 
-  //to send the data of students login 
-
-  const user=async (req,res)=>{
-    try{
-     
-          res.status(200).json("user data is safe, Mohan");
-    }
-    catch(error){
-      console.log(`error from user path ${error}`);
-    }
+const home = async (req, res) => {
+  try {
+    res.status(200).send("welcome Mohan");
+  } catch (error) {
+    console.log(error);
   }
-  export default { home, register, login, user };
+};
+const register = async (req, res) => {
+  try {
+    res.status(200).json("hey Mohan ,welcome to registration page");
+  } catch (error) {
+    res.status(500).json("hey Mohan ,internal server error");
+  }
+};
+const login = async (req, res) => {
+  try {
+    res.status(200).json("all right , Mohan");
+  } catch (error) {
+    res.status(500).json("hey Mohan ,server error");
+  }
+};
+//to send the data of students login
+
+const user = async (req, res) => {
+  try {
+    res.status(200).json("user data is safe, Mohan");
+  } catch (error) {
+    console.log(`error from user path ${error}`);
+  }
+};
+export default { home, register, login, user };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from "express";
 import cors from "cors";
+import pathRoute from './router/path-router.js';
+import connectDB from './utils/db.js';
 // import cookieParser from "cookie-parser";
 const app = express();
 
 const corsValue={
   origin: process.env.CORS_ORIGIN,
-    credentials: true,
+  credentials: true,
 }
 app.use(cors(corsValue));
 app.use(express.json());
@@ -15,8 +17,6 @@ app.use(express.static("public"));
 // app.use(cookieParser());
 
 const port=process.env.PORT||8000;
-import pathRoute from './router/path-router.js';
-import connectDB from './utils/db.js';
 
 
 app.use("/",pathRoute);
