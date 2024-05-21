a4c3526ece5c142477def8d436e804fd5bc623c8
Yash <dubeycdk@gmail.com>
2024-05-21 19:57:05 +0530

    Connection Updated

--- server/index.js
+++ server/index.js
@@ -2,30 +2,29 @@ import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import pathRoute from './router/path-router.js';
-import connectDB from './utils/db.js';
+import connectDB from './connection.js';
 // import cookieParser from "cookie-parser";
 const app = express();
 
-const corsValue={
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-}
-app.use(cors(corsValue));
+// const corsValue={
+//   origin: process.env.CORS_ORIGIN,
+//   credentials: true,
+// }
+
+//Middlewares
+// app.use(cors(corsValue));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 // app.use(cookieParser());
 
 const port=process.env.PORT||8000;
+const URI=process.env.mongoDB_URI;
 
+//Database Connection
+connectDB(URI);
 
+//Router
 app.use("/",pathRoute);
-connectDB().then(() => {
-    app.listen(port,() => {
-      console.log(`Server is running at port : ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-  });
-  export default app;
+app.listen(port,() => console.log(`Server is running at port : ${port}`));
+  // export default app;
