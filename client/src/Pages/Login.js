a8740005580497fd06c82100dfaff7bf4487d753
Vipul Kumar Kushwaha <100346094+Kushwaha-vipul@users.noreply.github.com>
2024-05-27 10:42:39 +0530

    interface of login and register pages

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,5 +1,64 @@
-import React from 'react'
-function Login(){
-return <h1>welcome login page</h1>
+import { useState } from 'react';
+
+const Login =()=>{
+    const [user,setUser]=useState({
+        email:"",
+        password:"",
+    });
+
+    const handleInput=(e)=>{
+        let name=e.target.name;
+        let value=e.target.value;
+
+        setUser({
+            ...user,
+            [name]:value,
+
+        })
+    }
+      const handleSubmit= async(e)=>{
+        e.preventDefault();
+     
+      }
+    return (
+         <>
+    <section>
+     <main className="boxe">
+ <form onSubmit={handleSubmit}>
+   
+     <div className="inputBox">
+       <label htmlFor="email">email</label>
+       <input
+         type="email"
+         name="email"
+         id="email"
+         placeholder="type your email"
+         required
+         autoComplete="off"
+         value={user.email}
+         onChange={handleInput}
+       />
+     </div>
+    
+     <div className="inputBox">
+       <label htmlFor="password">Password</label>
+       <input
+         type="password"
+         name="password"
+         id="password"
+         placeholder="type your password"
+         required
+         autoComplete="off"
+         value={user.password}
+         onChange={handleInput}
+       />
+     </div>
+   
+     <button type="submit" style={{ float: 'left' }}>Login</button>
+ </form>
+     </main>
+    </section>
+     </>
+     )
 }
 export default Login;
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,5 +1,101 @@
-import React from 'react'
-function Register(){
-return <h1>welcome to register page</h1>
+import { useState } from 'react';
+
+
+ const Register =()=>{
+
+    const [user,setUser]=useState({
+        username:"",
+        email:"",
+        phone:"",
+        password:"",
+       
+
+    });
+
+   
+     const handleInput=(e)=>{
+        console.log(e);
+        let name=e.target.name;
+        let value=e.target.value;
+        setUser({
+          
+            ...user,
+          
+            [name]:value,
+
+        })
+     }
+
+
+     const handleSubmit =async(e)=>{
+            e.preventDefault();
+            console.log(user);
+          
+     }
+   return (
+    <>
+   <section>
+    <main className="boxe">
+<form onSubmit={handleSubmit}>
+    <div className="inputBox">
+    <label htmlFor="username">username</label>
+    <input
+    type="text"
+    name="username"
+    placeholder="username"
+    id="username"
+    required
+    autoComplete="off"
+    value={user.username}
+    onChange={handleInput}
+   />
+    </div>
+    <div className="inputBox">
+      <label htmlFor="email">email</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="type your email"
+        required
+        autoComplete="off"
+        value={user.email}
+        onChange={handleInput}
+      />
+    </div>
+    <div className="inputBox">
+      <label htmlFor="phone">Phone</label>
+      <input
+        type="number"
+        name="phone"
+        id="phone"
+        placeholder="type your phone"
+        required
+        autoComplete="off"
+        value={user.phone}
+        onChange={handleInput}
+      />
+    </div>
+    <div className="inputBox">
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="type your password"
+        required
+        autoComplete="off"
+        value={user.password}
+        onChange={handleInput}
+      />
+    </div>
+  
+    <button type="submit" style={{ float: 'left' }}>Signup</button>
+</form>
+    </main>
+   </section>
+    </>
+   )
 }
+
 export default Register;
