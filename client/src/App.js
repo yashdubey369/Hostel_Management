f896743bbe6cf264cfde8992560b022e5ab27677
Vipul Kumar Kushwaha <100346094+Kushwaha-vipul@users.noreply.github.com>
2024-05-27 16:28:05 +0530

    tsparticle use

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import 'devextreme/dist/css/dx.light.css';
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import NavBar from "./Components/NavBar";
 import Hostel from "./Pages/Hostel/Hostel";
@@ -13,39 +13,51 @@ import Attendance from "./Pages/Hostel/Attendance";
 import SideBar from "./Components/SideBar";
 import Apply from "./Pages/Hostel/Apply";
 import Profile from "./Pages/Hostel/Profile";
+import Particlesbg from "./Components/Particlesbg";
 
-function App() {
+const WrapContent=()=>{
+  const location=useLocation();
+  const ActiveParticles=location.pathname==="/login"||location.pathname==="/register";
+
   return (
-    <div className="App">
-      <Router>
-        <div className="container">
-          <div className="header">
-            <NavBar />
-          </div>
-          <div className="main">
-            <div className="left">
-              <SideBar/>
-            </div>
-            <div className="right">
-              <Routes>
-                <Route exact path="/" element={<HomePage />} />
-                <Route exact path="/register" element={<Register />} />
-                <Route exact path="/login" element={<Login />} />
-              <Route exact path="/complain" element={<Complain />} />
-                <Route exact path="/hostel/apply" element={<Apply />} />
-                <Route exact path="/hostel/attendance" element={<Attendance/>} />
-                <Route exact path="/mess" element={<Mess />} />
-                <Route exact path="/hostel/profile" element={<Profile/>}/>
-              </Routes>
-            </div>
-          </div>
-          <div className="footer">
-            <Footer />
-          </div>
-        </div>
-      </Router>
-    </div>
+    <>
+    {ActiveParticles && <Particlesbg/>}
+    
+             <Routes>
+               <Route exact path="/" element={<HomePage />} />
+               <Route exact path="/register" element={<Register />} />
+               <Route exact path="/login" element={<Login />} />
+               <Route exact path="/complain" element={<Complain />} />
+               <Route exact path="/hostel/apply" element={<Apply />} />
+               <Route exact path="/hostel/attendance" element={<Attendance/>} />
+               <Route exact path="/mess" element={<Mess />} />
+               <Route exact path="/hostel/profile" element={<Profile/>}/>
+             </Routes>
+          
+    </>
   );
+};
+const App=()=>{
+  return(
+     <Router>
+         <div className="container">
+         <div className="header">
+           <NavBar />
+         </div>
+         <div className="main">
+           <div className="left">
+             <SideBar/>
+           </div>
+           <div className="right">
+          <WrapContent/>
+           </div>
+         </div>
+         <div className="footer">
+           <Footer />
+         </div>
+       </div>
+     </Router>
+  )
 }
 
 export default App;
---
+++ client/src/Components/Particlesbg.jsx
@@ -0,0 +1,95 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+
+import { loadSlim } from "tsparticles-slim"; 
+
+const Movie = () => {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+      
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#121010",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#79c0e1",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
+
+ export default Movie;
