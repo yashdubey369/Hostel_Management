91fd3c43989c441244a4988b5a6d4c541aac0e2b
Vipul Kumar Kushwaha <100346094+Kushwaha-vipul@users.noreply.github.com>
2024-05-29 00:31:35 +0530

    nothing

--- server/router/pathRouter.js
+++ server/router/pathRouter.js
@@ -2,11 +2,12 @@ import express from "express";
 const router = express.Router();
 import pathcontroller from "../controllers/pathController.js";
 import signup from "../validators/path-validator.js";
+import login from "../validators/path-validator.js";
 import validation from "../middlewares/signupMW.js";
 
 router.route("/").get(pathcontroller.home);
 router.route("/register").post(validation(signup),pathcontroller.register);
-router.route("/login").post(pathcontroller.login);
+router.route("/login").post(validation(login),pathcontroller.login);
 router.route("/user").get(pathcontroller.user);
 
 export default router;
--- server/validators/path-validator.js
+++ server/validators/path-validator.js
@@ -11,7 +11,11 @@ const signup=z.object({
     .trim()
     .min(3,{message:"email is at least 3 character"})
     .max(50,{message:"email is at max 30 character"})
-    .email({ message: "Please enter a valid email address" }),
+    .email({ message: "Please enter a valid email address" })
+    .refine((value) => {
+        // Custom refinement to check for "@" and "."
+        return value.includes("@") && value.includes(".");
+      }, { message: "Please enter a valid email address" }),
   
     phone:z
     .string({required_error:"phone  required"})
@@ -25,4 +29,23 @@ const signup=z.object({
     .max(20,{message:"password is at max 30 digit"}),
    
 });
-export default signup;
+const login=z.object({
+   
+    email:z
+    .string({required_error:"email  required"})
+    .trim()
+    .min(3,{message:"email is at least 3 character"})
+    .max(50,{message:"email is at max 30 character"})
+    .email({ message: "Please enter a valid email address" })
+    .refine((value) => {
+        // Custom refinement to check for "@" and "."
+        return value.includes("@") && value.includes(".");
+      }, { message: "Please enter a valid email address" }),
+    password:z
+    .string({required_error:"password  required"})
+    .trim()
+    .min(4,{message:"password is at least 4 digit"})
+    .max(20,{message:"password is at max 30 digit"}),
+   
+});
+export default {signup,login};
